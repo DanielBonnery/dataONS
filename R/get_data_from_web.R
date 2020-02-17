0bcbad67977a1a921118fd5ffe11d1f3cff2f686
DanielBonnery <daniel.bonnery@gmail.com>
2020-02-17 11:36:50 +0000

    No information

--- R/get_data_from_web.R
+++ R/get_data_from_web.R
@@ -1,33 +1,34 @@
+#' Downloads the file parish110217popest.zip from internet
+#' 
+#' @param directory a character string  indicating a file path (default: file.path(find.package("dataONS"),'data')) otherwise 
+#' @param datasets.to.download a vector of character strings, (default:c("parish110217popest")). If null, the files parish110217popest.zip are downloaded.
+#' 
+#' @details this function is executed when loading the package for the first time.
+#' @examples 
+#' attach(dataONS::get_data_from_web(directory=NULL))# takes some time to dowload data
+#' 
 get_data_from_web<-function(directory=if(file.exists(try(file.path(find.package("dataONS"),'data')))){
-  file.path(find.package("dataONS"),'data')}else{NULL}){
+  file.path(find.package("dataONS"),'data')}else{NULL},
+  datasets.to.download=c("parish110217popest")){
+  L<-lapply(datasets.to.download,function(dataset.to.download){
+    if(dataset.to.download=="parish110217popest"){
+      parish110217popest<-get_parish110217popest.zip()
+      if(!is.null(directory)){save(parish110217popest,file = file.path(directory,"parish110217popest.rda"))}
+      if(is.null(directory)){parish110217popest}else{NULL}
+    }})
+  names(L)<-datasets.to.download
+  L}
+  
+
+#' Download, unzip and read the file parish110217popest.zip from internet
+#' 
+#' @details download, unzip and read the file "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/adhocs/009305populationestimatesforparishesinenglandandwalesmid2002tomid2017/parish110217popest.zip"
+
+get_parish110217popest.zip<-function(){
   webfile="https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/adhocs/009305populationestimatesforparishesinenglandandwalesmid2002tomid2017/parish110217popest.zip"
   tmpf  <-tempfile()
-  xxxx=try(download.file(url=data.url,destfile = tmpf,method="wget",extra="--random-wait --retry-on-http-error=503"))
-  Sys.sleep(sample(20, 1))
-  while(is.element("try-error",class(xxxx))){
-      warning(paste0("Have to try to download ",data.url," again, previous attempt failed"))
-      download.file(url=data.url,destfile = tmpf,method="wget",extra="--random-wait --retry-on-http-error=503")
-      Sys.sleep(sample(20, 1))
-    }
-    Sys.sleep(sample(10, 1))
-    x=unzip(tmpf,exdir = tempdir())
-    y<-read.fwf(x,width=format.table$length,
-                header=FALSE,
-                colClasses=c("character","numeric","numeric")[format.table$class],fill=TRUE)
-    y<-y[format.table$variable!=""]
-    names(y)<-format.table$variable[format.table$variable!=""]
-    reformat(y)}
-  L1<-lapply(c("07cempst.zip","09empst.zip","10empst.zip","11empst.zip","12cempst.zip"),get_data_from_webs,
-         format.table=read.csv(system.file("extdata","data_format.csv",package="dataASPEP")))
-  L2<-lapply(c("07cempid.zip","09empid.zip","10empid.zip","11empid.zip","12cempid.zip"),get_data_from_webs,
-         format.table=read.csv(system.file("extdata","id_format.csv",package="dataASPEP")))
-  names(L1)<-paste0("aspep",years)
-  names(L2)<-paste0("aspep",years,"_gov")
-  listofids<-unique(unlist(lapply(L1,function(x){x$id})))
-  attach(L1);attach(L2)
-  if(!is.null(directory)){
-    cat(paste(paste0("aspep",outer(years,c("","_gov"),paste0)),collapse="\n"),file=file.path(directory,'datalist'))}
-  for (x in c(names(L1),names(L2))){
-    savetopackage=TRUE
-    eval(parse(text=paste0(x,"$id=factor(",x,"$id,levels=listofids);if(savetopackage){save(",x,",file=file.path(directory,'/",x,".rda'))}")))}
-  if(!savetopackage){return(c(L1,L2))}}
+  xxxx=downloader::download(webfile,
+                            destfile = tmpf,
+                            extra="--random-wait --retry-on-http-error=503")
+  x=unzip(tmpf,exdir = tempdir())
+  openxlsx::read.xlsx(x,sheet = 4,colNames=TRUE)}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Data: Annual Survey of Public Employment & Payroll
 
-`dataASPEP` is an R package that contains data from the Annual Survey of Public Employment & Payroll.
+`dataONS` is an R package that contains data from the Annual Survey of Public Employment & Payroll.
 The data was pulled from the [Census website](http://www.census.gov/govs/apes/).
 
 
@@ -9,7 +9,7 @@ The data was pulled from the [Census website](http://www.census.gov/govs/apes/).
 To install  the package, execute:
 
 ```r
-devtools::install_github("DanielBonnery/dataASPEP")
+devtools::install_github("DanielBonnery/dataONS")
 ```
 Note that installation is slow, because part of the installation process is the downloading of data from the Census Bureau website.
 
@@ -17,16 +17,12 @@ Note that installation is slow, because part of the installation process is the 
 ##Documentation
 To see the documentation, execute:
 
-```r
-?aspep2007
-?aspep2007_gov
-```
 
 ##How was the data pulled ?
 To pull the data again, execute:
 
 ```r
-alldata<-dataASPEP::get_data_from_web()
+alldata<-dataONS::get_data_from_web()
 ```
 
 ##How to use the data ?
@@ -34,5 +30,5 @@ To see an example of data use, execute:
 
 
 ```r
-demo(map,package = "dataASPEP")
+demo(map,package = "dataONS")
 ```
